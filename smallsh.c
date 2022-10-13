ea9ba193150f4681925f6234787cc545a758cfe5
Jason Carter <carterja@os1.engr.oregonstate.edu>
2022-10-13 13:28:50 -0700

    add cd

--- smallsh.c
+++ smallsh.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 
 
-void parseInput(char*[]);
+void parseInput(char*[], pid_t, int*);
 
 /* 
  * main() processes the input and executes the appropraite function
@@ -15,12 +15,11 @@ void parseInput(char*[]);
 int main(void) {
   
   char *args[512];
-  
+  pid_t pid = getppid(); 
   // Start loop
   while(1) {
-
-    parseInput(args);
-    
+    int argc = 0;
+    parseInput(args, pid, &argc);
     // If comment or no input continue
     if (args[0][0] == "#" || args[0][0] == '\0') {
       continue;
@@ -29,8 +28,21 @@ int main(void) {
     if (!strcmp(args[0], "exit")) {
       exit(0);
     }
+    
+    char cwd[FILENAME_MAX];
+    if (!strcmp(args[0], "cd")) {
+      if (argc > 1) {
+        if (chdir(args[1]) == -1) {
+        } else {
+          chdir(args[1]);
+          }
 
-    //Reset input
+      } else {
+        chdir(getenv("HOME"));
+      }
+    }
+
+    //Reset input and vars
     for (int i = 0; args[i]; i++) {
       args[i] = NULL;
     }
@@ -41,7 +53,7 @@ int main(void) {
 /*
  * Prompts the user and parses input into an array of arguments
  */
-void parseInput(char *args[]) {
+void parseInput(char *args[], pid_t pid, int *argc) {
   
   // Max input length defined at 2048 chars
   char input[2048];
@@ -59,11 +71,24 @@ void parseInput(char *args[]) {
     return;
   }
 
+  // replace any instance of $$ with pid
+  for (int i = 0; i < strlen(input); i++) {
+    if (input[i] == '$' && input[i+1] == '$') {
+      char *temp = strdup(input);
+      temp[i] = '%';
+      temp[i+1] = 'd';
+      sprintf(input, temp, pid);
+      free(temp);
+    }
+  }
+
   // Parse input into tokens and save command and arguments in args array
   char *token = strtok(input, " ");
   for (int i = 0; token; i++) {
-    args[i] = strdup(token);
+    args[i] = strdup(token); 
+    *argc += 1;
 
     token = strtok(NULL, " ");
   }
+
 }
