0514a95df0df3ac3d105d3ad776998b914997c97
Jason Carter <carterjasona@gmail.com>
2022-10-21 14:15:14 -0700

    update make

--- README.txt
+++ README.txt
@@ -2,4 +2,6 @@ Jason Carter
 CS344 - Smallsh
 Fall 2022
 
-Compile the programming by running 'make.' See Makefile for additional details.
+Compile the programming by running 'make' or with the command:
+
+gcc -g -o smallsh smallsh.c smallsh_lib.c linkedlist.c -O3 -Wextra -Wpedantic 
--- smallsh.h
+++ smallsh.h
@@ -4,7 +4,7 @@
 #define MAX_ARGS 512
 #define MAX_IN 2048
 
-
+// define linked list node
 typedef struct node {
     pid_t val;
     struct node *next;
@@ -44,7 +44,7 @@ void runExternalCommand(char*[], int*, int*, char*, char*, node**);
 
 void handleSIGTSTP(int signo);
 
-
+// Linked list methods for keeping track of background PIDs
 void insert_node(pid_t val, node **head);
 
 void remove_node(node **head, pid_t val);
