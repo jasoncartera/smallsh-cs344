07a35cede9a8dfb95b41c5c5eb1950e99e9b1bdc
Jason Carter <carterja@os1.engr.oregonstate.edu>
2022-10-21 15:03:01 -0700

    formatting

--- smallsh.h
+++ smallsh.h
@@ -13,24 +13,25 @@
  *  pid:           shell pid
  *  argc:          number of arguments passed
  *  isBackground:  checks if command should be a background process or not
- *  inFile:        pointer to string to store the name of inFile if there is an input redirect
- *  outFile:       pointer to string to store the name of outFile if there is an output redirect
+ *  inFile:        pointer to string to store the name of inFile if there is an
+ * input redirect outFile:       pointer to string to store the name of outFile
+ * if there is an output redirect
  */
 
-void parseInput(char*[], pid_t, int*, int*, char**, char**);
+void parseInput(char *[], pid_t, int *, int *, char **, char **);
 
 /*
  * Runs commands other than exit, cd, and status
  *
  * params:
  *  args:         array of arguments
- *  exitStatus:   exitStatus of process 
+ *  exitStatus:   exitStatus of process
  *  isBackground: check if command should be executed as background process
  *  inFile:       name of inFile if there is an input redirect
  *  outFile:      name of outFile if there is an output redirect
  */
 
-void runExternalCommand(char*[], int*, int*, char*, char*, pid_t[], int*);
+void runExternalCommand(char *[], int *, int *, char *, char *, pid_t[], int *);
 
 /* Customer handler for the SIGTSTP signal
  * Code modified from class Module 5
@@ -38,5 +39,4 @@ void runExternalCommand(char*[], int*, int*, char*, char*, pid_t[], int*);
 
 void handleSIGTSTP(int signo);
 
-#endif 
-
+#endif
